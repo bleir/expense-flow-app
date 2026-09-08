47fdba1c471c25056cbdb3590075898f03eed420
Adam Salej <adamsalej@gmail.com>
2026-09-08 23:01:48 +0200

    fix Heading title on dark theme

--- frontend/components/Heading.tsx
+++ frontend/components/Heading.tsx
@@ -3,7 +3,7 @@ import { HeadingProps } from "@/types";
 export default function Heading({ title, children }: HeadingProps) {
   return (
     <section className="mb-6">
-      <h2 className="text-2xl font-semibold text-gray-800">{title}</h2>
+      <h2 className="text-2xl font-semibold text-foreground">{title}</h2>
       <p className="text-muted-foreground">{children}</p>
     </section>
   );
